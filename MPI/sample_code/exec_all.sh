500ebf128d0ac85eec9894b073442003b6c9ebbe
Kengo Miyamoto <126746379+Kengo-Miyamoto@users.noreply.github.com>
2026-04-17 15:13:25 +0900

    Improve MPI sample cleanup and submit scripts
    Make cleanup and submission scripts more robust and safer: combine rm calls with -f, switch backticks to $(...), quote variable expansions and paths, use find -delete (and -executable -delete) instead of -exec rm, and fix the directory glob pattern ([a-rt-z]*). Also quote sbatch arguments and echo full commands before running. These changes reduce failures on spaces, avoid errors when files are missing, and improve overall script reliability.

--- MPI/sample_code/exec_all.sh
+++ MPI/sample_code/exec_all.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -x
 #
-rm *.stdout
-rm *.stderr
-for i in `find . -name "[a-z]*" -type d`
+rm -f *.stdout *.stderr
+for i in $(find . -name "[a-z]*" -type d)
 do
-  find ${i} -name ${i##*/} -type f -executable -print -exec rm {} \;
+  find "${i}" -name "${i##*/}" -type f -executable -delete
 done
-find . -name "exec.sh_[1-9]*.*" -print -exec rm {} \;
-find . -name "*[0-9].txt" -print -exec rm {} \;
-find . -name "*[0-9].dat" -print -exec rm {} \;
-find . -name "*_m.dat" -print -exec rm {} \;
-find . -name "*.tmp" -print -exec rm {} \;
+find . -name "exec.sh_[1-9]*.*"  -delete
+find . -name "*[0-9].txt" -delete
+find . -name "*[0-9].dat" -delete
+find . -name "*_m.dat" -delete
+find . -name "*.tmp" -delete
 #
 bash submit_all.sh testq 1> submit_all.sh.stdout 2> submit_all.sh.stderr
 #
