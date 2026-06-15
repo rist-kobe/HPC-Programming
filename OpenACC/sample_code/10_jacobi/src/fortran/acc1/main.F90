5c92428dc91b4c6bd09ed23ce11eafc832ee454d
Yukihiro Ota <yota@rist.or.jp>
2026-06-15 11:10:03 +0900

    Add files via upload

--- OpenACC/sample_code/10_jacobi/src/fortran/acc1/main.F90
+++ OpenACC/sample_code/10_jacobi/src/fortran/acc1/main.F90
@@ -1,8 +1,8 @@
-! Copyright 2024 Research Organization for Information Science and Technology
+! Copyright 2026 Research Organization for Information Science and Technology
 ! /*--------------------------------------------------------------------
 !  Title:       Jacobi method (2-dim. model, dynamical memory allocation)
 !  Author:      Yukihiro Ota (yota@rist.or.jp)
-!  Last update: 31st Jan. 2024
+!  Last update: June 15th, 2026
 !  Reference:   
 !    [1] M. Sugihara and K. Murota, "Theoretical Numerical Linear 
 !    Algebra" (Iwanami,2009) [in Japanese].
@@ -72,8 +72,8 @@ program main
   lconv = .false.
   do itr =  1, MAXITR
 
+    ! Update phio except for bounraries
     nrmsq = 0.0_DP
-
     !$ACC data copyin(PHIE(1:NX,1:NY),RHO(1:NX,1:NY)) copyout(PHIO(1:NX,1:NY))
     !$ACC kernels
     !$ACC loop independent collapse(2) reduction(max:nrmsq)
@@ -88,7 +88,12 @@ program main
     end do
     !$ACC end kernels
     !$ACC end data
-#if 0
+
+    ! Bounrary condition  
+    !   Note: This procedure seems to be redundant whenever using 
+    !   Dirichlet boundary condition with the values of zero.
+    !   We keep this implementation so that one can change it to
+    !   more general cases.
     do ix = 1, NX
       PHIO(ix,1) = 0.0_DP
       PHIO(ix,NY) = 0.0_DP
@@ -98,8 +103,8 @@ program main
       PHIO(1,iy) = 0.0_DP
       PHIO(NX,iy) = 0.0_DP
     end do
-#endif
 
+    ! Copy for the next step 
     !$ACC data copyin(PHIO(1:NX,1:NY)) copyout(PHIE(1:NX,1:NY))
     !$ACC kernels
     !$ACC loop 
