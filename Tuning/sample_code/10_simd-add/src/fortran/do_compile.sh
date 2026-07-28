140cc1c6262f119c5499a49e4153602bcbe405b8
Kengo Miyamoto <126746379+Kengo-Miyamoto@users.noreply.github.com>
2026-07-28 11:50:11 +0900

    Remove Intel sections from do_compile.sh, keep GNU only

--- Tuning/sample_code/10_simd-add/src/fortran/do_compile.sh
+++ Tuning/sample_code/10_simd-add/src/fortran/do_compile.sh
@@ -13,34 +13,3 @@ mv -v *.x ${dirname}/
 make COMPILER=${compiler} veryclean
 } &> ${compiler}.log
 mv ${compiler}.log ${dirname}
-
-# Intel oneAPI 2022.3.1
-module purge
-module load intel_oneapi/2022.3.1
-compiler=intel
-dirname=${compiler}
-mkdir -p ${dirname}
-{
-ifx --version
-make COMPILER=${compiler}
-mv -v *.x ${dirname}/
-mv -v *.yaml ${dirname}/
-mv -v *.optrpt ${dirname}/
-make COMPILER=${compiler} veryclean
-} &> ${compiler}.log
-mv ${compiler}.log ${dirname}
-
-# Intel oneAPI 2022.3.1 (Classic)
-#module purge
-#module load intel_oneapi/2022.3.1
-#compiler=intel-classic
-#dirname=${compiler}
-#mkdir -p ${dirname}
-#{
-#icx --version
-#make COMPILER=${compiler}
-#mv -v *.x ${dirname}/
-#mv -v *.optrpt ${dirname}/
-#make COMPILER=${compiler} veryclean
-#} &> ${compiler}.log
-#mv ${compiler}.log ${dirname}
