f5274264afceb4eae48ce47e45db1497e377205b
Kengo Miyamoto <126746379+Kengo-Miyamoto@users.noreply.github.com>
2026-08-14 09:57:51 +0900

    Enhance run.sh to output profiler data
    Added commands to display profiler data outputs.

--- Tuning/sample_code/03_prof-ex/tests/c/run.sh
+++ Tuning/sample_code/03_prof-ex/tests/c/run.sh
@@ -13,9 +13,11 @@ ${EXE} 0 > diffuse.out
 
 # Get profiler data 
 sleep 10s
+echo
 gprof -p ${EXE} > flat.out
-
+cat flat.out
+echo
 gprof -q ${EXE} > call.out
-
+cat call.out
 echo -n "END: " 
 date
