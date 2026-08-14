a256a0e931fb8f43248cfd3d68777c561307f095
Kengo Miyamoto <126746379+Kengo-Miyamoto@users.noreply.github.com>
2026-08-14 10:10:06 +0900

    Enhance run.sh with additional echo statements
    Added echo statements for better output formatting.

--- Tuning/sample_code/03_prof-ex/tests/c/run.sh
+++ Tuning/sample_code/03_prof-ex/tests/c/run.sh
@@ -7,7 +7,6 @@ ulimit -s unlimited
 # Set load module (absolute path could be preferable)
 EXE=$(echo "../../src/c/diffuse.x")
 
-
 # Run
 ${EXE} 0 > diffuse.out
 
@@ -17,7 +16,9 @@ echo
 gprof -p ${EXE} > flat.out
 cat flat.out
 echo
+echo
 gprof -q ${EXE} > call.out
 cat call.out
 echo -n "END: " 
 date
+echo
