b67245be1d791b67bb0fd1a63f636a4f5a853b20
Kengo Miyamoto <126746379+Kengo-Miyamoto@users.noreply.github.com>
2026-08-14 18:17:19 +0900

    Use MODE variable in run.sh instead of comment toggling; remove unnecessary blank lines

--- Tuning/sample_code/01_timer/tests/c/run.sh
+++ Tuning/sample_code/01_timer/tests/c/run.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+## Select timer mode: bash run.sh MODE=elp | cpu | gprof  (default: elp)
+## Or: MODE=gprof bash run.sh
+for arg in "$@"; do
+  case $arg in
+    MODE=*) MODE="${arg#MODE=}" ;;
+  esac
+done
+MODE=${MODE:-elp}
+
+case $MODE in
+  elp|cpu|gprof) ;;
+  *) echo "Unknown MODE '$MODE'. Use MODE=elp, MODE=cpu, or MODE=gprof" >&2; exit 1 ;;
+esac
+
 echo -n "START: " 
 date
 ulimit -s unlimited
@@ -7,15 +21,15 @@ ulimit -s unlimited
 # Set load module (absolute path could be preferable)
 EXE=$(echo "../../src/c/run.x")
 
-
-
 # Run
 $EXE > outfile
 
-# Get profiler data 
-# If setting -pg in the compiler option, uncomment the following two lines
-#sleep 10s
-#gprof $EXE > prof.out
+# Get profiler data
+# Executed only when the program was built with -pg (make MODE=gprof)
+if [ "$MODE" = "gprof" ]; then
+  sleep 10s
+  gprof $EXE > prof.out
+fi
 
 echo -n "END: " 
 date
