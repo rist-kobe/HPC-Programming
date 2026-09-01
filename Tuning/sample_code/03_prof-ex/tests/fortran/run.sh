d9d4196b25ccf249473f078b7c2aeb05f8485b70
Kengo Miyamoto <126746379+Kengo-Miyamoto@users.noreply.github.com>
2026-09-01 12:19:22 +0900

    Add files via upload

--- Tuning/sample_code/03_prof-ex/tests/fortran/run.sh
+++ Tuning/sample_code/03_prof-ex/tests/fortran/run.sh
@@ -8,14 +8,21 @@ ulimit -s unlimited
 EXE=$(echo "../../src/fortran/diffuse.x")
 
 
+# Remove any stale profiling data and reports before run
+rm -f gmon.out flat.out call.out
+
 # Run
 ${EXE} 0 &> diffuse.out
 
-# Get profiler data 
-sleep 10s
-gprof -p ${EXE} > flat.out
+# Get profiler data
+if [ ! -f gmon.out ]; then
+    echo "gmon.out not found. Rebuild with 'make MODE=gprof' and re-run." >&2
+else
+    sleep 10s
+    gprof -p ${EXE} gmon.out > flat.out
 
-gprof -q ${EXE} > call.out
+    gprof -q ${EXE} gmon.out > call.out
+fi
 
 echo -n "END: " 
 date
