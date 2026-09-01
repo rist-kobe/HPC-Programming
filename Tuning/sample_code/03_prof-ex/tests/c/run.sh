4c8741826826cf48d1307888b714679a9125d046
Kengo Miyamoto <126746379+Kengo-Miyamoto@users.noreply.github.com>
2026-09-01 12:18:16 +0900

    Add files via upload

--- Tuning/sample_code/03_prof-ex/tests/c/run.sh
+++ Tuning/sample_code/03_prof-ex/tests/c/run.sh
@@ -7,15 +7,20 @@ ulimit -s unlimited
 # Set load module (absolute path could be preferable)
 EXE=$(echo "../../src/c/diffuse.x")
 
+# Remove any stale profiling data and reports before run
+rm -f gmon.out flat.out call.out
 
 # Run
 ${EXE} 0 > diffuse.out
 
-# Get profiler data 
-sleep 10s
-gprof -p ${EXE} > flat.out
-
-gprof -q ${EXE} > call.out
-
+# Get profiler data
+if [ ! -f gmon.out ]; then
+    echo "gmon.out not found. Rebuild with 'make MODE=gprof' and re-run." >&2
+else
+    sleep 10s
+    gprof -p ${EXE} gmon.out > flat.out
+    gprof -q ${EXE} gmon.out > call.out
+fi
 echo -n "END: " 
 date
+echo
